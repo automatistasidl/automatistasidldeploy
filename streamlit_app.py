bd7c3921042e3967f9aef58207bdac7f94888040
automatistasidl <automatistasidl@gmail.com>
2025-06-02 15:52:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Título da página
-st.title("Consultar SKU")
-
-# Campo de entrada de texto
-sku = st.text_input("Digite o SKU", key="sku_input")
-
-# Mostrar o valor digitado
-if sku:
-    st.write(f"Você digitou: {sku}")
-
 # Inserir HTML com JavaScript para focar no campo de entrada de SKU
 html_code = """
     <html>
