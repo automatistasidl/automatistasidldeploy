e2ecc3a9d8b4c186601e7709e2469455e144a962
automatistasidl <automatistasidl@gmail.com>
2025-06-02 12:54:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
-# Definir o título da página
+# Título da página
 st.title("Consultar SKU")
 
-# Criar o campo de entrada de texto para SKU
+# Campo de entrada
 sku = st.text_input("Digite o SKU", key='sku_input')
 
-# Se o SKU for inserido, você pode exibir algo
+# JavaScript para focar automaticamente no primeiro campo de input
+st.markdown("""
+    <script>
+        const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+        window.onload = async function() {
+            await sleep(100);  // Aguarda um pouco para garantir que o input foi carregado
+            const input = window.parent.document.querySelector('input');
+            if (input) {
+                input.focus();
+            }
+        }
+    </script>
+""", unsafe_allow_html=True)
+
+# Exibir resultado se o SKU for digitado
 if sku:
     st.write(f"Você digitou: {sku}")
