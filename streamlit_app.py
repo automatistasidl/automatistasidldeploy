6e2b3917a9cb2b85fd4871c4bd12d9f65166942c
automatistasidl <automatistasidl@gmail.com>
2025-06-02 12:52:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,6 @@ st.title("Consultar SKU")
 # Criar o campo de entrada de texto para SKU
 sku = st.text_input("Digite o SKU", key='sku_input')
 
-# Injetar o JavaScript para dar foco ao campo de input
-st.markdown("""
-    <script>
-        window.onload = function() {
-            document.getElementById('sku_input').focus();
-        }
-    </script>
-""", unsafe_allow_html=True)
-
 # Se o SKU for inserido, você pode exibir algo
 if sku:
     st.write(f"Você digitou: {sku}")
