12f1f459931b22949a93995202bfea4f609f5525
automatistasidl <automatistasidl@gmail.com>
2025-06-02 14:15:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 sku = st.text_input("Digite o SKU", key="sku_input")
 
 # Título da página
-st.result("Consultar SKU")
+st.title("Consultar SKU")  # Corrigido de st.result para st.title
 
 # Mostrar valor digitado
 if sku:
