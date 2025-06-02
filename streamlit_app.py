76986af22dfa3189b366f7cbbf2b03a71d337fd2
automatistasidl <automatistasidl@gmail.com>
2025-06-02 14:20:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
+# Definir o título
+st.title("Consultar SKU")
+
+# Usar JavaScript para dar foco automático
+st.markdown("""
+    <script>
+    window.onload = function() {
+        document.getElementById("sku_input").focus();
+    }
+    </script>
+""", unsafe_allow_html=True)
+
 # Campo de entrada de texto
 sku = st.text_input("Digite o SKU", key="sku_input")
-
-# Título da página
-st.title("Consultar SKU")  # Corrigido de st.result para st.title
 
 # Mostrar valor digitado
 if sku:
