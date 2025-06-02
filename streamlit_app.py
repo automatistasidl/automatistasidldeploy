94d5cd01149c52432de1519d3a548839caaf2ec3
automatistasidl <automatistasidl@gmail.com>
2025-06-02 14:35:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,26 @@ st.title("Consultar SKU")
 # Campo de entrada de texto
 sku = st.text_input("Digite o SKU", key="sku_input")
 
+# Adicionar código JavaScript para focar o campo sem abrir o teclado
+st.markdown("""
+    <script>
+        function focusWhithoutKeyboard(inputElement) {
+            inputElement.setAttribute('readonly', 'readonly');
+            inputElement.focus();
+            setTimeout(function() {
+                inputElement.removeAttribute('readonly');
+            }, 100);
+        }
+
+        window.onload = function() {
+            var inputElement = document.getElementById("sku_input");
+            if (inputElement) {
+                focusWhithoutKeyboard(inputElement);
+            }
+        }
+    </script>
+""", unsafe_allow_html=True)
+
 # Mostrar o valor digitado
 if sku:
     st.write(f"Você digitou: {sku}")
