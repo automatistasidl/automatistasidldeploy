abffcd5d9f25bfe5509beb3be8ff4ae7a702c261
automatistasidl <automatistasidl@gmail.com>
2025-06-04 13:10:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import json
+import streamlit.components.v1 as components
 
 # Inicializa a tabela no session_state
 if 'skus' not in st.session_state:
